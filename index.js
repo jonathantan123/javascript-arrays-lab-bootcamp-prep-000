2d8c6b6d2300805ff922f3ad67b0926323a2dd32
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-06 13:37:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-
-// Add your functions and code here
 function destructivelyAppendKitten(name) {
- return kittens.push(name)
+  kittens.push(name) 
+  return Array
 }
 
 function destructivelyPrependKitten(name) {
-  return kittens.unshift(name)
-}
-
-function destructivelyRemoveLastKitten() {
-  return kittens.pop()
-}
-
-function destructivelyRemoveFirstKitten() {
-  return kittens.shift()
+  kittens.unshift(name)
+  return Array
 }
 
 function appendKitten(name) {
-  return [...kittens, name]
-}
-
-function prependKitten(name) {
-  return [name,...kittens] ; 
-  }
-   
- function removeLastKitten() {
-  return kittens.slice(0,kittens.length-1)
- }
-  
-  function removeFirstKitten() {
-    return  kittens.slice(1)
-  }
+ return [... kittens, name] }
+ 
